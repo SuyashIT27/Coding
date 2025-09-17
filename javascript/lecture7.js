a72b3af0dbdb132ef9a79fad08b6bf41078c4902
Suyash Verma <suyashvermaabes27@gmail.com>
2025-09-17 09:38:23 +0530

    Add files via upload

---
+++ javascript/lecture7.js
@@ -0,0 +1,8 @@
+//ATTRIBUES
+//getAttribute(attr) :-to get the attribute value.
+//setAttribute(attr,Value) :-to set the attribute val th
+let id = document.querySelectorAll("div");
+
+let id1 = id.getAttribute("class");
+console.dir(id1.inner);
+
