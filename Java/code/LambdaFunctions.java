7b5cc07748adcd83e47bf227889b3c44d63a082b
Suyash Verma <suyashvermaabes27@gmail.com>
2026-02-25 09:09:38 +0530

    Add files via upload

---
+++ Java/code/LambdaFunctions.java
@@ -0,0 +1,17 @@
+/*1. Shortcuts: Lambda expressions are
+quick, nameless functions for small
+tasks.
+2. Syntax: Written as (parameters)->
+{body}, linking inputs to actions.
+3. Functional Interfaces: They work
+with interfaces that have only one
+method, making code concise.
+4. Readability: They make code shorter
+and clearer, especially with
+collections.
+5. Useful with Collections: Great for
+managing lists and sets, like filtering
+or sorting.*/
+
+
+
